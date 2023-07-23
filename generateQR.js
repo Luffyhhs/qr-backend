d21011d95aac8fa9d21237779b060f487e529f9f
Luffyhhs <heinhtet1290@gmail.com>
2023-07-24 02:19:09 +0630

    first

---
+++ generateQR.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+const qr = require("qrcode");
+
+// Generate Payload
+function generatePayload(
+  pim,
+  merchantID,
+  transactionAmount,
+  merchantName,
+  merchantCity,
+  countryCode,
+  mcc,
+  currencyCode,
+  tipIndicator,
+  rfuEmvCo,
+  merchantInfo
+) {
+  const payload = {
+    pim,
+    merchantID,
+    transactionAmount,
+    merchantName,
+    merchantCity,
+    countryCode,
+    mcc,
+    currencyCode,
+    tipIndicator,
+    rfuEmvCo,
+    merchantInfo,
+  };
+
+  return JSON.stringify(payload);
+}
+
+// Generate QR Code
+function generateQRCode(payload, outputPath) {
+  const writeStream = fs.createWriteStream(outputPath);
+  const qrCode = qr.toFileStream(writeStream, payload);
+  // qrCode.pipe(writeStream);
+  writeStream.on("finish", () => {
+    console.log(`QR code saved: ${outputPath}`);
+  });
+}
+
+// Generate Random Merchant ID
+function generateMerchantID() {
+  const randomID = Math.floor(Math.random() * 10000000000).toString();
+  return randomID;
+}
+
+// Read merchant data from JSON file
+
+const { merchants } = JSON.parse(
+  fs.readFileSync("client/src/assets/merchantDatas.json")
+);
+
+// Generate QR Codes for each merchant
+merchants.forEach((merchant) => {
+  const pim = "00";
+  const merchantID = generateMerchantID();
+  const transactionAmount = "100.00";
+  const countryCode = "MM";
+  const mcc = "1234";
+  const currencyCode = "MMK";
+  const tipIndicator = "01";
+  const rfuEmvCo = "0000";
+  const merchantInfo = {
+    templateID: "26",
+    guid: "D840000000",
+  };
+
+  const { merchantName, merchantCity } = merchant;
+
+  const payload = generatePayload(
+    pim,
+    merchantID,
+    transactionAmount,
+    merchantName,
+    merchantCity,
+    countryCode,
+    mcc,
+    currencyCode,
+    tipIndicator,
+    rfuEmvCo,
+    merchantInfo
+  );
+
+  const fileName = `client/src/assets/${merchantName
+    .replace(/\s/g, "_")
+    .toLowerCase()}_qr_code.png`;
+  const outputPath = fileName;
+  generateQRCode(payload, outputPath);
+
+  console.log(`QR Code generated for ${merchantName}`);
+});
+
+module.exports = { generateQRCode };
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const qr = require("./generateQR");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Handle EMV RQ requests
+app.post("/emv-rq", (req, res) => {
+  // Extract data from the request
+  const reqData = req.body;
+  console.log(reqData);
+  const filePath = "client/src/assets/merchantdatas.json";
+
+  if (fs.existsSync(filePath)) {
+    fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log("error reading json file:", err);
+      } else {
+        let newData = [];
+        if (data) {
+          const { merchants } = JSON.parse(data);
+          newData = [...merchants, ...reqData];
+        } else {
+          newData = [...reqData];
+        }
+
+        fs.writeFile(
+          filePath,
+          JSON.stringify({ merchants: newData }),
+          (err) => {
+            if (err) {
+              console.error("error writing json file:", err);
+              res.status(500).json({ error: "Error writing Json file" });
+            } else {
+              res.json({
+                message: "json file saved",
+              });
+            }
+          }
+        );
+      }
+    });
+    // generateQRCode();
+  } else {
+    fs.writeFile(filePath, JSON.stringify({ merchants: reqData }), (err) => {
+      if (err) {
+        console.error("error writing json file:", err);
+        res.status(500).json({ error: "Error writing Json file" });
+      } else {
+        res.json({
+          message: "json file saved",
+        });
+      }
+    });
+
+    // generateQRCode();
+  }
+
+  // Process the EMV RQ data
+  // Replace this with your own logic
+
+  // Prepare the response
+  // const response = {
+  //   result: "success",
+  //   message: "EMV RQ processed successfully",
+  // };
+
+  // // Send the response
+  // res.json(response);
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
